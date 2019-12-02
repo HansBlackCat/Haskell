cd23013bc45cde27b45ef93752f1ca55c8a13f28
HansBlackCat <jeremy3599@snu.ac.kr>
2019-12-03 01:33:25 +0900

    Desk_191203

--- func/baby.hs
+++ func/baby.hs
@@ -13,3 +13,26 @@ circumference r = 2*pi*r
 
 circumference' :: Double -> Double
 circumference' r = 2*pi*r
+
+lucky :: Int -> String
+lucky 7 = "LUCKY SEVEN"
+lucky x = "Be-Beep!"
+
+factorial' :: Int -> Int
+factorial' 0 = 1
+factorial' n = n * factorial' (n-1)
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors (x1,y1) (x2,y2) = (x1 + x2, y1 + y2)
+
+trd :: (a, b, c) -> c
+trd (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Empty list"
+head' (x: _) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "Empty list"
+tell (x:[]) = "Element: " ++ show x
+tell (x:y:[]) = "Elements: " ++ show x ++ ", "  ++ show y
