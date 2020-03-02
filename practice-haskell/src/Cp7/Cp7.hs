f642401d0c314ed648d14aa04d733da2780e43b9
HansBlackCat <jeremy3599@snu.ac.kr>
2020-03-02 21:22:48 +0900

    2020-03-02 21:22:40

--- practice-haskell/src/Cp7/Cp7.hs
+++ practice-haskell/src/Cp7/Cp7.hs
@@ -4,7 +4,7 @@ module Cp7.Cp7 where
 
 import Data
 
-import Control.Monad 
+import Control.Monad
 import Control.Lens
 import Data.List
 import Control.Monad.Reader
@@ -16,19 +16,19 @@ import qualified Data.Set as S
 -- Apriori Algorithm
 -- ------------------------------------------------------------------------
 clientToPurchaseInfo :: Client -> Set PurchaseInfo
-clientToPurchaseInfo = 
-  (\case GovOrg c      -> S.fromList [InfoClientKind KindGovOrg]
-         Company c p d -> S.fromList [InfoClientKind KindCompany, InfoClientDuty d]
-         Individual p  -> S.fromList [InfoClientKind KindIndividual])
+clientToPurchaseInfo =
+  \case GovOrg _      -> S.fromList [InfoClientKind KindGovOrg]
+        Company _ _ d -> S.fromList [InfoClientKind KindCompany, InfoClientDuty d]
+        Individual _  -> S.fromList [InfoClientKind KindIndividual]
 
 productsToPurchaseInfo :: [Product] -> Set PurchaseInfo
-productsToPurchaseInfo = foldr 
+productsToPurchaseInfo = foldr
   (\(Product i t) pinfos -> S.insert (InfoPurchasedProduct i) $
                             S.insert (InfoPurchasedProductType t) pinfos)
   S.empty
 
 purchaseToTransaction :: Purchase -> Transaction
-purchaseToTransaction (Purchase c p) = 
+purchaseToTransaction (Purchase c p) =
   Transaction $ clientToPurchaseInfo c `S.union` productsToPurchaseInfo p
 
 -- DEBUG
@@ -47,20 +47,20 @@ newtype FrequentSet = FrequentSet (Set PurchaseInfo)
 data AssocRule = AssocRule (Set PurchaseInfo) (Set PurchaseInfo)
                deriving (Eq, Ord)
 
-instance Show AssocRule where 
-  show (AssocRule s1 s2) = show s1 ++ " => " ++ show s2 
+instance Show AssocRule where
+  show (AssocRule s1 s2) = show s1 ++ " => " ++ show s2
 
 
 
-setSupport :: [Transaction] -> FrequentSet -> Double 
+setSupport :: [Transaction] -> FrequentSet -> Double
 setSupport trans (FrequentSet sElts) =
   let total = length trans
       f (Transaction tElts) = sElts `S.isSubsetOf` tElts
       supp = length $ filter f trans
   in fromIntegral supp / fromIntegral total
 
-ruleConfidence :: [Transaction] -> AssocRule -> Double 
-ruleConfidence trans (AssocRule a b) = 
+ruleConfidence :: [Transaction] -> AssocRule -> Double
+ruleConfidence trans (AssocRule a b) =
   setSupport trans (FrequentSet $ a `S.union` b)
   / setSupport trans (FrequentSet a)
 
@@ -68,7 +68,7 @@ generateL1 :: Double -> [Transaction] -> [FrequentSet]
 generateL1 minSupport transactions =
   noDups $ do Transaction t <- transactions
               e <- S.toList t
-              let fs = FrequentSet $ S.singleton e 
+              let fs = FrequentSet $ S.singleton e
               guard $ setSupport transactions fs > minSupport
               return fs
 
