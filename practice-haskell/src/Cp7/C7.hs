57e394f56e2ca5afc3d043a277d11a53639486d6
HansBlackCat <jeremy3599@snu.ac.kr>
2020-03-04 22:55:45 +0900

    2020-03-04 22:55:37

--- practice-haskell/src/Cp7/C7.hs
+++ practice-haskell/src/Cp7/C7.hs
@@ -2,14 +2,17 @@ module Cp7.C7 where
 
 import Data
 
-import Control.Monad 
+import Control.Monad
 import Control.Lens
 import Data.List
 import Control.Monad.Reader
 import Control.Monad.Writer hiding (Product)
 import Data.Set (Set)
 import qualified Data.Set as S
+
 import Control.Monad.State
+import System.Random
+import Control.Applicative
 
 -- monad transormers
 
@@ -55,30 +58,6 @@ find_ f l =
   let listJusted = map (\x -> Just x) l
 -}
 
--- ---------------------------------------------------------------------
--- State Practice
--- https://wiki.haskell.org/State_Monad
-type GameValue = Int
-type GameState = (Bool, Int)
-playGame :: String -> State GameState GameValue
-playGame [] = do
-  (_, score) <- get
-  return score
-playGame (x:xs) = do
-    (on, score) <- get
-    case x of
-         'a' | on -> put (on, score + 1)
-         'b' | on -> put (on, score - 1)
-         'c'      -> put (not on, score)
-         _        -> put (on, score)
-    playGame xs
-
-startState :: GameState
-startState = (False, 0)
-
-testState :: IO ()
-testState = print $ evalState (playGame "abcabcabcbaaabbcbbabcbabcbc") startState
-
 -- Writer Practice
 data Entry = InEntry { id :: Int
                      , msg :: String }
@@ -90,3 +69,184 @@ logMsg f = tell [InEntry 20 f]
 -- WriterT (Identity ((),[InEntry {id = 20, msg = "as"}]))
 -- λ> runWriter $ logMsg "as"
 -- ((),[InEntry {id = 20, msg = "as"}])
+
+-- ---------------------------------------------------------------------
+-- State
+-- ---------------------------------------------------------------------
+-- State 1
+-- https://en.wikibooks.org/wiki/Haskell/Understanding_monads/State
+
+-- randomRIO :: Random a => (a, a) -> IO a
+-- randomIO  :: Random a => IO a
+
+-- randomR(1,6) :: StdGen -> (Int, StdGen)
+rollDie :: State StdGen Int
+rollDie = state $ randomR (1,6)
+
+-- state :: (s -> (a, s)) -> m a
+-- get   :: m s
+-- put   :: s -> m ()
+
+-- evalState :: State s a -> s -> a
+-- runState  :: State s a -> s -> (a,s)
+{-
+rollDie' :: State StdGen Int
+rollDie' = do generator <- get
+             let (value, newGenerator) = randomR (1,6) generator
+             put newGenerator
+             return value
+-}
+
+rollDice :: State StdGen (Int, Int)
+rollDice = liftA2 (,) rollDie rollDie
+
+-- random :: (Random a , RandomGen g) => g -> (a, g)
+getRandom :: Random a =>State StdGen a
+getRandom = state random
+
+allRandoms :: State StdGen (Int, Float, Char, Integer, Double, Bool, Int)
+allRandoms = (,,,,,,) <$> getRandom
+                      <*> getRandom
+                      <*> getRandom
+                      <*> getRandom
+                      <*> getRandom
+                      <*> getRandom
+                      <*> getRandom
+-- evalState allRandoms (mkStdGen 43)
+
+-- State 2
+-- https://wiki.haskell.org/State_Monad
+
+-- runstate (return 'X') 1 == ('X',1)
+{-
+return 'X' --with init 1--            :: State Int Char
+runState (return 'X') --with init 1-- :: Int -> (Char, Int)
+init, final state == 1                :: Int
+final value == 'X'                    :: Char
+-}
+
+-- runState get 1 == (1,1)
+{-
+get                                   :: State Int Int
+runState get                          :: Int -> (Int, Int)
+init, final state == 1                :: Int
+final value == 1                      :: Int
+-}
+
+-- runState (put 5) 1 == ((),5)
+{-
+put 5                                 :: State Int ()
+runState (put 5)                      :: Int -> ((), Int)
+init state == 1                       :: Int
+final state == 5                      :: Int
+final value == ()                     :: ()
+-}
+
+-- runState (do {put 5; return 'X'}) 1 == ('X',5)
+{-
+do {put 5; return 'X'}                :: State Int Char
+runState (do {put 5; return 'X'})     :: Int -> (Char, Int)
+init state == 1                       :: Int
+-}
+
+-- runState (do {put "Hola"; return 1}) "Hello" == (1,"Hola")
+
+-- runState (do {x <- get; put (x+1); return x}) 5 == (5,6)
+
+-- runState (do {x <- get; put (x-1); get}) 1 == (0,0)
+
+-- runState (modify (+10)) 1 == ((),11)
+-- runState (gets (+5)) 1 == (6,1)
+-- evalState (gets (+5)) 1 == 6
+-- execState (gets (+5)) 1 == 1
+
+{-
+x = 3
+x = x+1
+print(x)
+
+print((x+1))
+print: p, (+1): g, allocate func: f
+p.g.f 1
+-}
+
+{- (result, state)
+(return 5) 1 => (5,1)
+(put 5) 1    => ((),5)
+(get 5)      => (5,5)
+-}
+
+-- runState (do {x<-get;put (x+10);get;modify (*10);return 0}) 5 == (0,150)
+-- (5,5) -> ((),15) -> (15,15) -> ((),150) -> (0,150)
+
+type GameValue = Int
+type GameState = (Bool, Int)
+
+-- State State@(Bool,Int) Value@(Int) ~ runState::(Int,(Bool,Int))
+playGame :: String -> State GameState GameValue
+playGame []     = do
+    (_, score) <- get
+    return score
+
+playGame (x:xs) = do
+    (on, score) <- get
+    case x of
+         'a' | on -> put (on, score + 1)
+         'b' | on -> put (on, score - 1)
+         'c'      -> put (not on, score)
+         _        -> put (on, score)
+    playGame xs
+
+startState :: (Bool, Int)
+startState = (False, 0)
+
+mainGame :: IO ()
+mainGame = print $ evalState (playGame "abcaaacbbcabbab") startState
+
+type MyState = Int
+type MyStateMonad = State MyState
+
+valFromState :: MyState -> Int
+valFromState s = -s
+nextState :: MyState->MyState
+nextState x = 1+x
+
+getNext :: MyStateMonad Int
+getNext = state (\s -> let s'=nextState s in (valFromState s', s'))
+
+inc3Sugared ::MyStateMonad Int
+inc3Sugared = getNext >> getNext >> getNext
+
+inc3Sugared' ::MyStateMonad Int
+inc3Sugared' = do { getNext;getNext;getNext }
+
+-- evalState inc3Sugared  0 == -3
+-- evalState inc3Sugared' 0 == -3
+
+data MyStateType = MST Int Bool Char Int deriving Show
+getStateAny :: (Random a) => State StdGen a
+getStateAny = do g <- get
+                 (x,g') <- return $ random g
+                 put g'
+                 return x
+getStateOne :: (Random a) => (a,a) -> State StdGen a
+getStateOne bounds = do g <- get
+                        (x,g') <- return $ randomR bounds g
+                        put g'
+                        return x
+makeRandomValueST :: StdGen -> (MyStateType, StdGen)
+makeRandomValueST = runState (do n <- getStateOne (1,100)
+                                 b <- getStateAny
+                                 c <- getStateOne ('a','z')
+                                 m <- getStateOne (-n,n)
+                                 return (MST n b c m))
+
+-- ---------------------------------------------------------------------
+-- Writer
+-- ---------------------------------------------------------------------
+
+data EntryW = LogW { countW :: Int
+                   , msgW :: String }
+                 deriving Show
+logMsgW :: String -> Writer [EntryW] ()
+logMsgW s = tell [LogW 1 s]
