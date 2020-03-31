afa05acb67a505acc7455565f6d515b3c201c72e
HansBlackCat <jeremy3599@snu.ac.kr>
2020-03-31 17:06:49 +0900

    2020-03-31 17:06:28

--- practice-haskell/src/Sudoku/Sudoku2.hs
+++ practice-haskell/src/Sudoku/Sudoku2.hs
@@ -1,29 +1,84 @@
 module Sudoku.Sudoku2 where
 
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector as V
 import Data.List
-import Control.Monad
+import Data.Bits
+import Data.Word
 
-cross a b = [(x,y)|x<-a,y<-b]
+-- ---------------------------------------------------------------------------
+-- Base data
+-- ---------------------------------------------------------------------------
+type Base = Word32
+type Space = U.Vector Base
 
-solve f = foldr (=<<) [f] $
-  do
-    a <- lSmallGrid
-    b <- lSmallGrid
-    guard (f (a, b) == 0)
-    let ps = cross [a] lSmallGrid
-          <> cross lSmallGrid [b]
-          <> cross (cross [fst a] [0..2]) (cross [fst b] [0..2])
-    pure $ \g -> do
-        c <- [1..9] \\ map g ps
-        pure $ \p -> if p == (a, b) then c else g p
-  where lSmallGrid = cross [0..2] [0..2]
+totCellNums :: Int
+totCellNums = 81
 
-get s ((a,b), (c,d)) = map (read . (:[])) s !! (3*a + b + 9*(3*c + d))
+-- ---------------------------------------------------------------------------
+-- Tools
+-- ---------------------------------------------------------------------------
+-- is 2^28?
+newSingle :: Base -> Bool
+newSingle n = shiftR n 27 == 2
 
-put f = putStrLn $ unlines [concat [' ': show (f (a,b)) | a<-cross [0..2] [0..2]] | b<-cross [0..2] [0..2]]
+setSolution :: Base -> Base
+setSolution n = setBit n 27
 
-main = mapM_ put . solve $ get
- "060080204000002000802100007750004080000000003310009060405600001000003000070040609"
+maskChoices :: Base -> Base
+maskChoices n = n .&. 0x07FFFFFF
 
-readI :: String -> Integer
-readI = read
+countBits :: Base -> Base
+countBits 0 = 0
+countBits n = (cBLH 16 0xFFFF . cBLH 8 0xFF00FF . cBLH 4 0x0F0F0F0F . cBLH 2 0x33333333 . cBLH 1 0x55555555) n
+  where cBLH s mask n = (n .&. mask) + (n `shiftR` s) .&. mask
+
+minus :: Base -> Base -> Base
+minus x y
+  | maskChoices (x .&. y) == 0 = x
+  | otherwise = z .|. (shiftL (countBits z) 28)
+    where z = maskChoices $ x .&. (complement y)
+
+-- ---------------------------------------------------------------------------
+-- Settings
+-- ---------------------------------------------------------------------------
+rows :: Space -> Space
+rows = id
+
+columns :: Space -> Space
+columns vec = U.map (U.unsafeIndex vec) loc
+  where loc = U.fromList [r*9+c | c<-[0..8], r<-[0..8]]
+
+subGrids :: Space -> Space
+subGrids vec = U.map (U.unsafeIndex vec) loc
+  where loc = U.fromList [i+bc+br | br<-[0,27,54], bc<-[0,3,6], i<-[0,1,2,9,10,11,18,19,20]]
+
+boolSpace :: V.Vector (U.Vector Bool)
+boolSpace = V.generate totCellNums discriminator
+  where
+    discriminator idx1 = U.zipWith3 (\r c s -> r||c||s) sameR sameC sameG
+      where
+        sameR = U.generate totCellNums (\idx2 -> idx1 `div` 9 == idx2 `div` 9)
+        sameC = U.generate totCellNums (\idx2 -> idx1 `mod` 9 == idx2 `mod` 9)
+        sameG = U.generate totCellNums (\idx2 -> subGrid idx1 == subGrid idx2)
+          where
+            subGrid idx = (idx `div` 27, (idx `div` 3) `mod` 3)
+
+--
+zeroOptimize :: Space -> Maybe Space
+zeroOptimize space = U.foldM baseOptimize space $ U.findIndices newSingle space
+
+baseOptimize :: Space -> Int -> Maybe Space
+baseOptimize space index =
+  let sIndex = space U.! index
+      bIndex = boolSpace V.! index
+      newSpace = U.zipWith opt bIndex space
+        where opt a b
+                | a&&(b==sIndex) = setSolution sIndex
+                | a = minus b sIndex
+                | otherwise = b
+  in undefined
+
+-- Debug
+testRBase = 32 :: Base
+testRSpace = U.fromList [0..81] :: Space
